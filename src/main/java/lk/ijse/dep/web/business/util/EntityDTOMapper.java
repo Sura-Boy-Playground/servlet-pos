3cbf80e648edafde9e6103b88d6d2d8fbae6fe20
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-02-06 10:35:37 +0530

    Revamp with MapStruct

---
+++ src/main/java/lk/ijse/dep/web/business/util/EntityDTOMapper.java
@@ -0,0 +1,43 @@
+package lk.ijse.dep.web.business.util;
+
+
+import lk.ijse.dep.web.dto.CustomerDTO;
+import lk.ijse.dep.web.dto.ItemDTO;
+import lk.ijse.dep.web.entity.Customer;
+import lk.ijse.dep.web.entity.Item;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.util.List;
+
+@Mapper
+public interface EntityDTOMapper {
+
+    EntityDTOMapper instance = Mappers.getMapper(EntityDTOMapper.class);
+
+//    default EntityDTOMapper getInstance(){
+//        return Mappers.getMapper(EntityDTOMapper.class);
+//    }
+
+    Customer getCustomer(CustomerDTO dto);
+
+    CustomerDTO getCustomerDTO(Customer customer);
+
+    List<CustomerDTO> getCustomerDTOs(List<Customer> customers);
+
+    Item getItem(ItemDTO dto);
+
+    ItemDTO getItemDTO(Item item);
+
+    List<ItemDTO> getItemDTOs(List<Item> item);
+
+//    Order getOrder(OrderDTO dto);
+}
+
+@Qualifier
+@Target(ElementType.METHOD)
+@interface Address {
+
+}
---
+++ src/main/java/lk/ijse/dep/web/business/util/OrderEntityDTOMapper.java
@@ -0,0 +1,50 @@
+package lk.ijse.dep.web.business.util;
+
+
+import lk.ijse.dep.web.dao.DAOFactory;
+import lk.ijse.dep.web.dao.DAOTypes;
+import lk.ijse.dep.web.dao.custom.CustomerDAO;
+import lk.ijse.dep.web.dto.OrderDTO;
+import lk.ijse.dep.web.dto.OrderDetailDTO;
+import lk.ijse.dep.web.entity.Customer;
+import lk.ijse.dep.web.entity.Order;
+import lk.ijse.dep.web.entity.OrderDetail;
+import lk.ijse.dep.web.entity.OrderDetailPK;
+import lk.ijse.dep.web.util.HibernateUtil;
+import org.hibernate.Session;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import javax.persistence.EntityManager;
+import java.sql.Date;
+
+@Mapper
+public interface OrderEntityDTOMapper {
+
+    OrderEntityDTOMapper instance = Mappers.getMapper(OrderEntityDTOMapper.class);
+
+    @Mapping(source = "orderId", target = "id")
+    @Mapping(source = ".", target = "date")
+    @Mapping(source = ".", target = "customer")
+    Order getOrder(OrderDTO dto);
+
+    default Date toDate(OrderDTO dto){
+        return Date.valueOf(dto.getOrderDate());
+    }
+
+    default Customer getCustomer(OrderDTO dto){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            return session.get(Customer.class, dto.getCustomerId());
+        }
+    }
+
+    @Mapping(source = ".", target = "orderDetailPK", qualifiedByName = "pk")
+    OrderDetail getOrderDetail(OrderDetailDTO dto);
+
+    @Named("pk")
+    default OrderDetailPK toOrderDetailPK(OrderDetailDTO dto){
+        return new OrderDetailPK(dto.getOrderId(), dto.getItemCode());
+    }
+}
